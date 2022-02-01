11ec6ea298f50a8e2c80f0f32bc1c377b99632ac
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-01 12:43:03 -0800

    added activities to parkdetailcard

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,7 +10,6 @@ export default function Home() {
     const fetchData = async () => {
       const data = await fetchParks();
       setParks(data);
-      // console.log(data);
       setLoading(false);
     };
     fetchData();
--- src/views/ParkDetail/ParkDetail.jsx
+++ src/views/ParkDetail/ParkDetail.jsx
@@ -6,18 +6,34 @@ import { fetchParkById } from '../../services/natparks';
 export default function ParkDetail() {
   const { parkId } = useParams();
   const [parkDetails, setParkDetails] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchParkById(parkId);
       setParkDetails(data);
+      setLoading(false);
     };
     fetchData();
   }, [parkId]);
 
+  if (loading) return <p>Loading...</p>;
+  console.log(parkDetails);
+
+  const { activities, fullName } = parkDetails;
+  console.log(activities);
+
   return (
     <div>
-      <h1>{parkDetails?.fullName}</h1>
+      <h1>{fullName}</h1>
+
+      {activities.map((activity) => {
+        return (
+          <div key={activity.id}>
+            <p>{activity.name}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
