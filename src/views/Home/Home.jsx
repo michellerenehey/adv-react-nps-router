c695e8f25aa57f055b3bbc34701988c1105903d2
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-02-02 16:51:35 -0800

    search kind of working

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,17 @@
 import { fetchParks } from '../../services/natparks';
 import { useEffect, useState } from 'react';
 import ParksList from '../../components/ParksList/ParksList';
-import Search from '../Search/Search';
+import { useHistory } from 'react-router-dom';
+// import Search from '../Search/Search';
 
 export default function Home() {
   const [parks, setParks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [startPage, setStartPage] = useState(0);
+  const [search, setSearch] = useState('');
+
+  const history = useHistory();
+
   // const [query, setQuery] = useState('');
 
   useEffect(() => {
@@ -28,11 +33,24 @@ export default function Home() {
     setLoading(true);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push(`/?q=${search}`);
+  };
+
   if (loading) return <p>Loading...</p>;
 
   return (
     <>
-      <Search />
+      {/* <Search /> */}
+      <div>
+        <h1>form form building a form</h1>
+        <form onSubmit={handleSubmit}>
+          <label>Search for a park:</label>
+          <input value={search} onChange={(e) => setSearch(e.target.value)} />
+          <button>Submit</button>
+        </form>
+      </div>
       <ParksList parks={parks} handleNext={handleNext} handlePrev={handlePrev} />
     </>
   );
--- src/views/Search/Search.jsx
+++ src/views/Search/Search.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useState } from 'react';
+// import { useHistory } from 'react-router-dom';
 
-export default function Search() {
-  const [search, setSearch] = useState('');
-  const history = useHistory();
+// export default function Search() {
+//   const [search, setSearch] = useState('');
+//   const history = useHistory();
 
-  const handleSubmit = () => {
-    history.push(`/?q=${search}`);
-  };
+//   const handleSubmit = () => {
+//     history.push(`/?q=${search}`);
+//   };
 
-  return (
-    <div>
-      <h1>form form building a form</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Search for a park:</label>
-        <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>form form building a form</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label>Search for a park:</label>
+//         <input value={search} onChange={(e) => setSearch(e.target.value)} />
+//         <button>Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
